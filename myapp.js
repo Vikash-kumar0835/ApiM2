b8c974e5af59a8f3c6da85acbbb1616911d7411a
Vikash-kumar0835 <vikash.kumar.phe16@itbhu.ac.in>
2021-05-14 16:56:45 +0530

    portchange

--- myapp.js
+++ myapp.js
@@ -23,10 +23,7 @@ app.get('/api/whoami',function(req,res){
 
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.listen(3000,()=>console.log("server started on port 3000...!"));
 
 
 
